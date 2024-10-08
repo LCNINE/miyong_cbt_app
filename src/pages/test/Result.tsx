5483bd2926c9fe1268717e1de764296269a9c735
hyunbin66 <hyunbin.choi@lcnine.kr>
2024-10-08 17:00:09 +0900

    d

--- src/pages/test/Result.tsx
+++ src/pages/test/Result.tsx
@@ -66,7 +66,7 @@ export default function Result() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
+    <div className="min-h-full flex items-center justify-center bg-white">
       <motion.div
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}

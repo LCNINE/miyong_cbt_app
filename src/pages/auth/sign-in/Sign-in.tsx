833c5dd3ffa16b9994cc1aa6ad389c4ec53fa9f0
hyunbin66 <hyunbin.choi@lcnine.kr>
2024-10-21 15:51:42 +0900

    sign-in,up canonical add

--- src/pages/auth/sign-in/Sign-in.tsx
+++ src/pages/auth/sign-in/Sign-in.tsx
@@ -15,8 +15,18 @@ export default function SignIn() {
       <Helmet>
         <title>미용필시시험/sign-in - 미용필기시험 로그인</title>
         <meta name="description" content="미용필기시험 로그인" />
-        <meta name="google-site-verification" content="LK2lMpCXPbmg_peIKBrco_0Rp_scYKp4Mn0u5yI6vCI" />
-        <meta name="naver-site-verification" content="dd4919f9da4dfbafdd79f35ed97505cf41418c50" />
+        <meta
+          name="google-site-verification"
+          content="LK2lMpCXPbmg_peIKBrco_0Rp_scYKp4Mn0u5yI6vCI"
+        />
+        <meta
+          name="naver-site-verification"
+          content="dd4919f9da4dfbafdd79f35ed97505cf41418c50"
+        />
+        <link
+          rel="canonical"
+          href="https://www.xn--ok0b94xilff7df2wpza.com/sign-in"
+        />
       </Helmet>
       <SignInForm />
       <div className="text-center mt-5">
--- src/pages/auth/sign-up/Sign-up.tsx
+++ src/pages/auth/sign-up/Sign-up.tsx
@@ -7,8 +7,18 @@ export default function SignUp() {
       <Helmet>
         <title>미용필시시험/sign-up - 미용필기시험 회원가입</title>
         <meta name="description" content="미용필기시험 로그인" />
-        <meta name="google-site-verification" content="LK2lMpCXPbmg_peIKBrco_0Rp_scYKp4Mn0u5yI6vCI" />
-        <meta name="naver-site-verification" content="dd4919f9da4dfbafdd79f35ed97505cf41418c50" />
+        <meta
+          name="google-site-verification"
+          content="LK2lMpCXPbmg_peIKBrco_0Rp_scYKp4Mn0u5yI6vCI"
+        />
+        <meta
+          name="naver-site-verification"
+          content="dd4919f9da4dfbafdd79f35ed97505cf41418c50"
+        />
+        <link
+          rel="canonical"
+          href="https://www.xn--ok0b94xilff7df2wpza.com/sign-up"
+        />
       </Helmet>
       <SignUpForm />
     </div>

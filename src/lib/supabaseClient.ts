0d2fa6b9d0f40dfa1f89c7aab368090abf2064bd
hyunbin66 <hyunbin.choi@lcnine.kr>
2024-11-04 13:08:13 +0900

    j

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,8 +2,7 @@
 import { Database } from "@/type/database.types";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://aqwfljjtpddgqznegtfu.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxd2Zsamp0cGRkZ3F6bmVndGZ1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNTUwNTAxNSwiZXhwIjoyMDQxMDgxMDE1fQ.C-rifI7V5uk1ejO61vmNe4e9xzoEsStPrBrL9cGzh9E";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey =import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);

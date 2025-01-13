78d150f893537c2411285fbfa039c93ff0fc9a92
hyunbin66 <hyunbin.choi@lcnine.kr>
2025-01-13 15:29:05 +0900

    description update 찐막막

--- src/pages/schedule/Schedule.tsx
+++ src/pages/schedule/Schedule.tsx
@@ -7,7 +7,7 @@ function ExamSchedulePage() {
     <div>
       <Helmet>
         <title>미용필기시험/ 미용필기시험 미용사자격증 연간일정</title>
-        <meta name="description" content="미용필기시험 연간일정, 2025년 미용사 자격증 일정" />
+        <meta name="description" content="미용시험 일정, 미용필기시험 연간 일정, 2025년 미용사 자격증 일정, 미용사 시험 달력" />
         <meta
           name="google-site-verification"
           content="LK2lMpCXPbmg_peIKBrco_0Rp_scYKp4Mn0u5yI6vCI"
